ff799234bb5630bd8e3509851368a4a1b1923fd2
Ling <airme1019@hotmail.com>
2016-10-20 22:24:35 -0700

    Create closures.js

---
+++ closures.js
@@ -0,0 +1,33 @@
+
+function test(){
+  var a = 6;
+ return function b(){
+    console.log(a);  
+  }
+  
+}
+
+var ii= test();
+ii();
+ 
+/*self invoke function */
+ var i = (function () {
+    var counter = 0;
+   
+      return function () {
+        return counter += 1;}
+ 
+  
+    
+})();
+console.log(i());
+ 
+var add = (function () {
+    var counter = 0;
+    return function () {
+      return counter += 1;}
+})();
+
+add();
+add();
+console.log(add());
