bbec288b51e28e2f9862f9550bc25133240c61db
Ling <airme1019@hotmail.com>
2018-04-06 20:37:09 -0700

    Add files via upload
    backup coding practices.

---
+++ leet_practice/isParantheseValid.js
@@ -0,0 +1,23 @@
+var isValid = function(s){
+    var s_arr = s.split('');
+    var ori_l = s_arr.length;
+    var close = [];
+    for(var i =0; i<ori_l; i++){
+      if(s_arr[i] == '{'){
+          close.push('}');         
+         }else if(s_arr[i] == '['){
+           close.push(']');
+         }else if(s_arr[i] == '('){
+           close.push(')');
+         }else{
+           if(s_arr[i] !== close.pop()){
+             return false;
+           }
+         }
+    }
+      if(close.length >0){
+        return false;             
+    }else{ return true;}  
+  
+};
+console.log(isValid('{[())]}'));
---
+++ leet_practice/test1.php
@@ -0,0 +1,3 @@
+<?php echo "hello";
+echo 'some change 2';
+?>
