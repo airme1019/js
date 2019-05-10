4fdc48c82d72cec4a5ff28f007d462f0a7c70611
Ling Zhao <lingzhao@apples-iMac.local>
2019-05-10 00:53:04 -0700

    5.10

---
+++ src/224caculator.js
@@ -0,0 +1,49 @@
+var calculate = function(s) {
+    let res = 0
+    let sign = 1
+    let n = 0
+    let stack = []
+    s = s.split('').filter(c => c !==' ').join('')
+    console.log(s)
+      for( let i = 0; i < s.length; i += 1) {
+      let c = s.charAt(i)
+      if(c >= '0' && c <= '9'){
+            let num = c
+            while(i+1 < s.length && s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9'){
+                num += s.charAt(i+1);
+                i++;
+            }
+        n = parseInt(num)
+console.log('n '+n)
+      } else if(c == '+'){
+        res += sign * n;
+            n = 0;
+            sign = 1;
+      } else if( c =='-'){
+        res += sign*n
+        sign = -1
+        n = 0
+         console.log('res '+res)
+      } else if( c == '('){
+        stack.push(res)
+        stack.push(sign)
+        res = 0
+        sign = 1
+      } else if( c == ')'){
+        res += sign*n
+        n = 0
+        res *= stack.pop()
+        res +=stack.pop()
+      } else{
+        n = parseInt(c)
+       }
+
+    }
+    console.log('res '+res)
+    console.log(n)
+    if(n != 0) {res += sign * n}
+    return res
+};
+
+const s =" 2-1 + 2 "
+console.log(calculate(s))
--- src/316removeDupLetters.js
+++ src/316removeDupLetters.js
@@ -1,25 +1,59 @@
 var removeDuplicateLetters = function(s) {
-  if(s.length == 0) return ''
-  let len = s.length
-  let stack = []
-  for ( let i = 0; i < len; i += 1) {
-    let c = s.charAt(i)
-    if(stack.includes(c)) {
-      let pos = stack.indexOf(c)
-
-          if(stack[pos] > stack[stack.length-1]) {
-            stack.splice(pos,1)
-            stack.push(c)
-          }
-     } else {
-      stack.push(c)
-    }
+  const m = new Array(26);
+  const a = 'a'.charCodeAt(0);
+  for (let i = 0; i < s.length; i++) {
+    const k = s.charCodeAt(i) - a;
+    m[k] = m[k] ? m[k]+1 : 1;
   }
-  console.log(stack)
-return stack.filter( c => c != 0).join('')
-};
-
+  const sol = [];
+  const visited = new Array(26);
+  for (let i = 0; i < s.length; i++) {
+    const k = s.charCodeAt(i) - a;
+    console.log(k)
+    m[k] = m[k] - 1;
+    if (visited[k])
+      continue;
+    while(sol.length > 0) {
+      const last = sol[sol.length-1]-a;
+      console.log('last: ' +last)
+      if (last > k && m[last] > 0) {
+        visited[last] = false;
+        sol.pop();
+      } else
+        break;
+    }
+    visited[k] = true;
+    sol.push(k+a);
+    //console.log(i,sol,m);
+  }
+  return String.fromCharCode(...sol);
+}
 const s = "bcabc"
 const s2 = "cbacdcbc"
-console.log(removeDuplicateLetters(s2))
-//console.log('f'-'b')
+//console.log(removeDuplicateLetters(s2))
+
+var removeDuplicateLetters1 = function(s) {
+  let occ = {}
+  for(let i = 0; i < s.length; i += 1) {
+    occ[s.charAt(i)] = occ[s.charAt(i)] ? occ[s.charAt(i)] + 1 : 1
+  }
+  console.log(occ)
+  let visited = {}
+  let res = []
+  for(let i = 0; i < s.length; i += 1) {
+    occ[s.charAt(i)] -= 1
+    if (visited[s.charAt(i)]) continue
+
+    while(res.length > 0) {
+      // last character > current character and there is more last character after current character
+      if(s.charAt(i) < res[res.length-1] && occ[res[res.length-1]] > 0){
+        visited[res[res.length-1]] = false
+        res.pop()
+      } else break
+    }
+    visited[s.charAt(i)] = true
+    res.push(s.charAt(i))
+  }
+  return res
+}
+console.log(removeDuplicateLetters1('cbacdcbc'))
