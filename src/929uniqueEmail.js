95d9ebdd3a8584f0443d920e4f0d17569568d2d9
Ling Zhao <lingzhao@apples-iMac.local>
2019-05-14 01:01:29 -0700

    5.13

--- src/929uniqueEmail.js
+++ src/929uniqueEmail.js
@@ -1,5 +1,5 @@
 /*https://leetcode.com/problems/unique-email-addresses/*/
 
 var numUniqueEmails = function(emails) {
-
+     return new Set(emails.map(mail => `${mail.split('@')[0].replace(/\+.*$|\./g, '')}@${mail.split('@')[1]}`)).size
 };
