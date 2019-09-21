04669ed5581acf56ef2732957695ceb15d46de97
Ling Zhao <lingzhao@apples-iMac.local>
2019-09-21 11:53:49 -0700

    Reverse string recursion.

--- src/1.js
+++ src/1.js
@@ -6,4 +6,30 @@ var getCode = (function() {
   };
 })();
 
-console.log(getCode());    // Returns the apiCode
+//console.log(getCode());    // Returns the apiCode
+
+function add(a, b , callback){
+setTimeout(function(){
+  console.log(`The sum of ${a} and ${b} is ${a+b}.` )
+},2000);
+callback();
+}
+
+// add() function is called with arguments given below
+/*add(5,6,function disp(){
+console.log('This must be printed after addition.');
+});*/
+
+var reverseString = function(s) {
+
+    if (s.length == 0) {
+        return;
+    }
+    var temp = s.shift();
+    reverseString(s);
+    s.push(temp);
+
+    return s
+};
+var t = reverseString(["a","b","c","d","e"])
+console.log(t)
