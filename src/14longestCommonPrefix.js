7267a04b97b161acf57a8edfc5afc10644f51a3d
Ling Zhao <lingzhao@apples-iMac.local>
2019-09-22 22:54:20 -0700

    9/22.

--- src/14longestCommonPrefix.js
+++ src/14longestCommonPrefix.js
@@ -1,19 +1,17 @@
 var longestCommonPrefix = function(strs) {
-    if(strs.length==0) return '';
-   // var sorted_strs = strs.sort(function(a,b){ return a.length -b.length});
-  var sorted_strs = strs.sort();
-  // console.log(sorted_strs);
-    var shortest = sorted_strs[0].split('');
-    var longest = sorted_strs[sorted_strs.length -1].split('');  
-    var pre ='';
-  
-    for(var i = 0; i<shortest.length;i++){  
-      if(shortest[i] == longest[i]){ 
-        pre += shortest[i]; 
-      }else{break;}
+  if(strs.length==0) return '';
+  let shortest = strs.reduce((shortest, str) => Math.min(shortest, str.length), Number.MAX_SAFE_INTEGER)
+  var pre ='';
+  for(let i = 0; i<shortest;i++){
+    for( let j =0; j < strs.length; j++) {
+      if(strs[j].charAt(i) !== strs[0][i]) {
+        return pre
+      }
     }
-  return pre;  
-};
+    pre += strs[0][i]
+  }
+  return pre
+}
 
-// console.log(longestCommonPrefix(["aaaaaaaaaaa","aaaaaaaab","aaaaaaaaa"]) );     
-// console.log(longestCommonPrefix(["aba","abc","ababa"]) );   
+// console.log(longestCommonPrefix(["aaaaaaaaaaa","aaaaaaaab","aaaaaaaaa"]) );
+  console.log(longestCommonPrefix(["aba","abc","ababa"]) );
