4399764ee4fb760a97a56e8c06eaf8174ebd16fb
Ling <ling_zhao@apple.com>
2019-10-14 08:22:18 -0700

    10/14.

--- src/binaryTree1levelOrderBottom.js
+++ src/binaryTree1levelOrderBottom.js
@@ -1,3 +1,5 @@
+
+
 var levelOrderBottom = function(root) {
     if(!root) return [];
     var reversed = [];
@@ -5,11 +7,11 @@ var levelOrderBottom = function(root) {
     function search(node,level){
       if(node){
         if(reversed.length<level){
-          //reversed.unshift([]);
-            reversed.push([]);
+          reversed.unshift([]);
+            //reversed.push([]);
         }
-       // reversed[reversed.length-level].push(node.val);
-          reversed[reversed.length-1].push(node.val);
+        reversed[reversed.length-level].push(node.val);
+        //  reversed[reversed.length-1].push(node.val);
         search(node.left,level+1);
         search(node.right,level+1);
       } else {return;}
@@ -17,4 +19,4 @@ var levelOrderBottom = function(root) {
     return reversed;
 };
 const t = [3,9,20,null,null,15,7]
-const output = [[3],[9,20],[15,7]]
+//const output = [[3],[9,20],[15,7]]
