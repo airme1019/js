2663c4e6f904741e99932afbd3db9f5fe35dc31b
Ling Zhao <lingzhao@apples-imac.lan>
2020-03-29 17:43:09 -0700

    3/29.

--- src/349IntersectionOfTwoArrays.js
+++ src/349IntersectionOfTwoArrays.js
@@ -1,9 +1,13 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
 var intersection = function(nums1, nums2) {
+  let n1Set = new Set(nums1)
+  return [...new Set(nums2.filter(n => n1Set.has(n)))]
+}
+const nums1 = [1,2,2,1]
+const nums2 = [2,2]
+console.log(intersection(nums1, nums2))
+
+
+var intersection2 = function(nums1, nums2) {
     const presenceMap = nums1.reduce((acc, el) => {
         acc[el] = true;
         return acc;
@@ -18,4 +22,4 @@ console.log(presenceMap);
         return acc;
     }, []);
 };
-console.log(intersection([1,1,2],[2,2,3]))
+console.log(intersection2([1,1,2],[2,2,3]))
